00024b40b7443bfb3f738b283f813d04f30953b0
Patric Oliveira <patric.eng.computation@gmail.com>
2023-01-05 15:09:51 -0300

    Using mailtrap.io for sending emails

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -264,7 +264,7 @@
 
             <h2>The small framework with powerful features</h2>
 
-            <a href="<?php echo route_to('manager'); ?>">Manager</a>
+            <a href="<?php echo route_to('manager') ?>">Manager</a>
 
         </div>
 
