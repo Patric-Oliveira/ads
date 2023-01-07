ad9c57139f66d62ea8bf11fb45c1aae89f9fe84c
Patric Oliveira <patric.eng.computation@gmail.com>
2023-01-07 15:45:40 -0300

    Configuring the Main Template

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,7 @@ $routes->setAutoRoute(false);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index', ['as' => 'home']);
+$routes->get('/', 'HomeController::index', ['as' => 'home']);
 
 // Rotas para o manager
 if (file_exists($manager = ROOTPATH . 'routes/manager.php')) {
