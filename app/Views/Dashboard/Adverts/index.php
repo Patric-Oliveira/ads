aabd6d48193b06363d19c2ab728fe166e66fa8a0
Patric Oliveira <patric.eng.computation@gmail.com>
2023-01-09 18:03:08 -0300

    Creating the Ads index view - part 2

--- app/Views/Dashboard/Adverts/index.php
+++ app/Views/Dashboard/Adverts/index.php
@@ -10,26 +10,48 @@
 <?= $this->section('content') ?>
 
 <section class="dashboard section">
-        <!-- Container Start -->
-        <div class="container">
-            <!-- Row Start -->
-            <div class="row">
+    <!-- Container Start -->
+    <div class="container">
+        <!-- Row Start -->
+        <div class="row">
 
             <?php echo $this->include('Dashboard/Layout/_sidebar') ?>
 
-                <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-0">
-                    <!-- Recently Favorited -->
-                    <div class="widget dashboard-container my-adslist">
-                        <h3 class="widget-header">Listando Meus Anúncios</h3>
+            <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-0">
+                <!-- Recently Favorited -->
+                <div class="widget dashboard-container my-adslist">
+                    <h3 class="widget-header">Listando Meus Anúncios</h3>
 
+                </div>
+
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="button" id="createAdvertBtn" class="btn btn-main-sm add-button float-right mb-2"><i class="fa fa-plus"></i> Criar</button>
+                        <table class="table table-borderless table-striped" id="dataTable">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Imagem</th>
+                                    <th scope="col" class="none">Código</th>
+                                    <th scope="col" class="all">Título</th>
+                                    <th scope="col" class="none text-center">Categoria</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col" class="none">Onde</th>
+                                    <th scope="col" class="all text-center">Ações</th>
+                                </tr>
+                            </thead>
+                        </table>
                     </div>
                 </div>
+
+
             </div>
-            <!-- Row End -->
         </div>
-        <!-- Container End -->
-    </section>
-    
+        <!-- Row End -->
+    </div>
+    <!-- Container End -->
+</section>
+
 <?= $this->endSection() ?>
 
 <?= $this->section('scripts') ?>
