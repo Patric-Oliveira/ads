80c4f23803a07a71ee0745e9a3c2caee27edd66b
Patric Oliveira <patric.eng.computation@gmail.com>
2023-01-09 12:46:02 -0300

    Preparing the Advert Model

--- app/Models/AdvertModel.php
+++ app/Models/AdvertModel.php
@@ -2,41 +2,82 @@
 
 namespace App\Models;
 
-use CodeIgniter\Model;
+use App\Entities\Advert;
 
-class AdvertModel extends Model
+class AdvertModel extends MyBaseModel
 {
+    private $user;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        /**
+         * @todo  $this->user = service('auth')->user() ?? auth('api')->user(); // alterar para api
+         */
+
+        $this->user = service('auth')->user();
+    }
+
     protected $DBGroup          = 'default';
     protected $table            = 'adverts';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
+    protected $returnType       = Advert::class;
+    protected $useSoftDeletes   = true;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = [
+        'user_id',
+        'category_id',
+        'code',
+        'title',
+        'description',
+        'price',
+        //'is_published', // nop aqui, pois queremos ter um controle maior de quando o anúncio deverá ser publicado/despublicado
+        'situation',
+        'zipcode',
+        'street',
+        'number',
+        'neighborhood',
+        'city',
+        'state',
+    ];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+   // Callbacks
+   protected $allowCallbacks = true;
+   protected $beforeInsert   = ['escapeDataXSS', 'generateCitySlug', 'generateCode', 'setUserID'];
+   protected $beforeUpdate   = ['escapeDataXSS', 'generateCitySlug'];
 
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+   protected function generateCitySlug(array $data): array
+   {
+       if (isset($data['data']['city'])) {
+           $data['data']['city_slug'] = mb_url_title($data['data']['city'], lowercase: true);
+       }
+       return $data;
+   }
+
+   protected function generateCode(array $data): array
+   {
+       if (isset($data['data'])) {
+           $data['data']['code'] = strtoupper(uniqid('ADVERT_', true));
+       }
+       return $data;
+   }
+
+   protected function setUserID(array $data): array
+   {
+       if (isset($data['data'])) {
+           $data['data']['user_id'] = $this->user->id;
+       }
+       return $data;
+   }
+
 }
